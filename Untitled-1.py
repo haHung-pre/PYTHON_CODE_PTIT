4bb62ec75c54e5eb9722bc1c2d7978bc169788a7
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2025-06-13 16:23:49 +0700

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,22 @@
+import math
+
+class PhanSo:
+    def __init__(self, tu, mau):
+        ucln = math.gcd(tu, mau)
+        self.tu = tu // ucln
+        self.mau = mau // ucln
+
+    def __add__(self, other):
+        tu = self.tu * other.mau + other.tu * self.mau
+        mau = self.mau * other.mau
+        return PhanSo(tu, mau)
+
+    def __str__(self):
+        return f"{self.tu}/{self.mau}"
+
+
+a, b, c, d = map(int, input().split())
+p = PhanSo(a, b)
+q = PhanSo(c, d)
+
+print(p + q)
