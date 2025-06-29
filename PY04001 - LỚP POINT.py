2e6e9a7c1a3eff19c50c950c01346042ccdf76d3
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2025-06-29 10:23:09 +0700

    Update PY04001 - LỚP POINT.py

--- PY04001 - LỚP POINT.py
+++ PY04001 - LỚP POINT.py
@@ -15,3 +15,17 @@ if __name__ == '__main__':
         p2 = Point(Decimal(arr[2]), Decimal(arr[3]))
         print(p1.distance(p2))
         t -= 1
+
+
+import math
+class Point:
+  def __init__(self,x,y):
+    self.a=x
+    self.b=y
+t=int(input())
+for i in range(t):
+  a1,b1,a2,b2=list(map(float,input().split()))
+  p1=Point(a1,b1)
+  p2=Point(a2,b2)
+  result=math.dist([p1.a,p1.b],[p2.a,p2.b])
+  print(f"{result:.4f}")
