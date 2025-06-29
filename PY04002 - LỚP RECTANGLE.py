a2e95c234ef3490685d41a08edb25b8b47590550
haHung-pre <146201685+haHung-pre@users.noreply.github.com>
2025-06-29 10:24:12 +0700

    Update PY04002 - LỚP RECTANGLE.py

--- PY04002 - LỚP RECTANGLE.py
+++ PY04002 - LỚP RECTANGLE.py
@@ -10,3 +10,26 @@ if int(arr[0]) > 0 and int(arr[1]) > 0:
     print('{} {} {}'.format(r.perimeter, r.area, r.color)) 
 else:
     print('INVALID')
+
+
+
+
+class Rectangle:
+    def __init__(self, w, h, color):
+        self.w = w
+        self.h = h
+        
+        self.color = color[0:1].upper() + color[1:].lower()
+
+    def output(self):
+        if self.w <= 0 or self.h <= 0:
+            print("INVALID")
+        else:
+            chu_vi = (self.w + self.h) * 2
+            dien_tich = self.w * self.h
+            print(chu_vi, dien_tich, self.color)
+
+
+a = input().split()
+rec = Rectangle(int(a[0]), int(a[1]), a[2])
+rec.output()
